e8b032ebae2af896c0902dcfa3beebd5a9dcf115
ishika2802 <ishika2001patel@gmail.com>
2026-05-05 20:48:56 +0530

    Created repository for Notfication, Notification template and preference repository

--- src/main/java/com/notificationservice/notification_service/repository/NotificationRepository.java
+++ src/main/java/com/notificationservice/notification_service/repository/NotificationRepository.java
@@ -1,11 +1,21 @@
 package com.notificationservice.notification_service.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import com.notificationservice.notification_service.enums.NotificationStatus;
+import com.notificationservice.notification_service.enums.NotificationType;
 import com.notificationservice.notification_service.model.Notification;
 
 @Repository
 public interface NotificationRepository extends MongoRepository<Notification, String> {
+    
+    Page<Notification> findByUserId(String userId, Pageable pageable);
+
+    Page<Notification> findByUserIdAndStatus(String userId, NotificationStatus status, Pageable pageable);
+
+    Page<Notification> findByUserIdAndType(String userId, NotificationType type, Pageable pageable);
 
 }
--- src/main/java/com/notificationservice/notification_service/repository/PreferenceRepository.java
+++ src/main/java/com/notificationservice/notification_service/repository/PreferenceRepository.java
@@ -1,5 +1,7 @@
 package com.notificationservice.notification_service.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,9 @@ import com.notificationservice.notification_service.model.NotificationPreference
 
 @Repository
 public interface PreferenceRepository extends MongoRepository<NotificationPreference, String> {
+
+    Optional<NotificationPreference> findByUserId(String userId);
+
+    
 
 }
